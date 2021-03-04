a3d435b5c1ac7834c689544d8c097813667e1c11
Conrad Haupt <conrad@conradhaupt.com>
2021-03-04 19:20:53 +0200

    Add itertools variant
    Suggested by @steve_quantum:
    https://twitter.com/steve_quantum/status/1367518223923609607

--- all_bitstrings.py
+++ all_bitstrings.py
@@ -1,9 +1,16 @@
 import numpy as np
+import itertools as it
 
 def all_bitstrings(size):
     bitstrings = np.ndarray((2**size, size), dtype=int)
     for i in range(size):
         bitstrings[:, i] = np.tile(np.repeat(np.array([0, 1]), 2 ** (size-i-1)), 2**i)
+    return bitstrings
+
+def all_bitstrings_it(size):
+    bitstrings = np.ndarray((2 ** size, size), dtype=int)
+    for i, _bstring in enumerate(it.product([0, 1], repeat=size)):
+        bitstrings[i, :] = _bstring
     return bitstrings
 
 def all_bitstrings_slow(size):
